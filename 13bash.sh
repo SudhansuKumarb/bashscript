e0d5c37dcbd591b3b8b52c5e36c5a75a01bd991b
sudhansu <sudhansukumar85@gmail.com>
2023-03-09 14:37:18 +0530

    commit rest of files

---
+++ 13bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+#if condition (greater than) is true  
+if [ 10 -gt 3 ];  
+then  
+echo "10 is greater than 3."  
+fi  
+  
+#if condition (greater than) is false  
+if [ 3 -gt 10 ];  
+then  
+echo "3 is not greater than 10."  
+fi 
